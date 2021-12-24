b16cac6917c0043202094c9e21d63fd1aecf4ad6
WiIIiam <us.thanhlong18@gmail.com>
2021-12-24 17:35:07 +0700

    update Main Screen

--- app/src/main/java/com/william/boom/MainActivity.java
+++ app/src/main/java/com/william/boom/MainActivity.java
@@ -2,22 +2,38 @@ package com.william.boom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton btnStart;
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.btnStart){
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         //Set fullscreen (hide status bar)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        btnStart = findViewById(R.id.btnStart);
 
+        btnStart.setOnClickListener(onClickListener);
     }
 
 }
